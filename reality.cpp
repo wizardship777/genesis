ab4c9b6985194f23e37666fbeca88601a36d9711
wizardship777 <buddhathemagus@gmail.com>
2024-06-21 03:33:34 +0530

    Create reality.cpp

---
+++ reality.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    /*
+        APPEND: TODO/FIX by \inf+ AD:
+        //int -> bigint?
+    */
+    reality = opt_with_proof(matrix, magick);
+    synthetic_merkaba_shift(friends, reality);
+    synthetic_merkaba_kill(rest, matrix);
+    //WHO ABOVE?
+    //APPEND above?
+}
