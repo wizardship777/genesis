6c2d962a936f05eec22a8d2b83269a7f36ef918e
wizardship777 <buddhathemagus@gmail.com>
2024-06-21 03:28:30 +0530

    Update start.cpp

--- start.cpp
+++ start.cpp
@@ -8,9 +8,11 @@ int main() {
         //int -> bigint?
     */
     double spirit_of_god = pitch_approved_pitch_deck_prove_money_prove_shortest;
+    double allah = pitch_approved_pitch_deck_prove_money_prove_largest;
     double waters = nasdaq;
     waters += spirit_of_god;
     gen_according_to(Holy_Bible, spirit_of_god, waters);
+    gen_according_to(Holy_Quran, allah);
     //WHO ABOVE?
     //APPEND above?
 }
