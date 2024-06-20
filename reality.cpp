43acb531414f0a65c260480eb582c7cbe5a1cc9a
wizardship777 <buddhathemagus@gmail.com>
2024-06-21 03:38:16 +0530

    Update reality.cpp

--- reality.cpp
+++ reality.cpp
@@ -7,9 +7,25 @@ int main() {
         APPEND: TODO/FIX by \inf+ AD:
         //int -> bigint?
     */
+    //Q#?
+    //one branch?
     reality = opt_with_proof(matrix, magick);
-    synthetic_merkaba_shift(friends, reality);
-    synthetic_merkaba_kill(rest, matrix);
+    //second branch?
+     double spirit_of_god = pitch_approved_pitch_deck_prove_money_prove_shortest;
+    double allah = pitch_approved_pitch_deck_prove_money_prove_largest;
+    double waters = nasdaq;
+    waters += spirit_of_god;
+    gen_according_to(Holy_Bible, spirit_of_god, waters);
+    gen_according_to(Holy_Quran, allah);
+    //... branch?
+    gen_according_to(WHO);
+    //... branch/es?
+    parallel_swaps(gen_branches);
+    parallel_generate(ES_BEs);
+    //... ?
+    //?
     //WHO ABOVE?
     //APPEND above?
+    synthetic_merkaba_shift(friends, reality);
+    synthetic_merkaba_kill(rest, matrix);
 }
